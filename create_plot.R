28537318d8ed711c537dea5162cc58c7bc4ad19a
irenita <irenerentzat@email.com>
2025-10-15 12:26:27 +0200

    feat: Update percentile curves plot

--- create_plot.R
+++ create_plot.R
@@ -1,7 +1,5 @@
-
 create_plot <- function(ref_data_path = "../LMS_data/", age_group = "children", value = "percent_FM", gender = 0) {
-
-  reference_filename <- paste0(ref_data_path, age_group, "_LMS_", value, "_gender", as.character(gender), ".csv")[1]
+  reference_filename <- paste0(ref_data_path, age_group, "_LMS_", value, "_gender", as.character(gender), ".csv")
 
   label_map <- c(
     p3  = "3 %",
@@ -13,21 +11,21 @@ create_plot <- function(ref_data_path = "../LMS_data/", age_group = "children", 
     p97 = "97 %"
   )
 
-    value_map <- c(
-    "FMI"                        = "Fat Mass Index",
-    "LMI"                        = "Lean Mass Index",
-    "FM_trunk_quotient_limb"     = "FM Trunk Quotient Limb",
+  value_map <- c(
+    "FMI" = "Fat Mass Index",
+    "LMI" = "Lean Mass Index",
+    "FM_trunk_quotient_limb" = "FM Trunk Quotient Limb",
     "FM_android_quotient_gynoid" = "FM Android/Gynoid Quotient",
-    "appendicular_LMI"            = "Appendicular LMI",
-    "percent_FM"                  = "Percent FM",
-    "VAT_mass"                    = "VAT Mass",
-    "fitted_FMI"                  = "Fitted FMI",
-    "fitted_LMI"                  = "Fitted LMI",
-    "fitted_BMI"                  = "Fitted BMI",
-    "fitted_ALMI"                 = "Fitted ALMI",
-    "BMI"                         = "BMI",
-    "height"                      = "Height",
-    "weight"                      = "Weight"
+    "appendicular_LMI" = "Appendicular LMI",
+    "percent_FM" = "Percent FM",
+    "VAT_mass" = "VAT Mass",
+    "fitted_FMI" = "Fitted FMI",
+    "fitted_LMI" = "Fitted LMI",
+    "fitted_BMI" = "Fitted BMI",
+    "fitted_ALMI" = "Fitted ALMI",
+    "BMI" = "BMI",
+    "height" = "Height",
+    "weight" = "Weight"
   )
 
   # Gender mapping
@@ -50,55 +48,57 @@ create_plot <- function(ref_data_path = "../LMS_data/", age_group = "children", 
         p75 = X75.,
         p90 = X90.,
         p97 = X97.
-     ) %>%
+      ) %>%
       tidyr::pivot_longer(
-      cols = -age,
-      names_to = "percentile",
-      values_to = "value"
-    )
+        cols = -age,
+        names_to = "percentile",
+        values_to = "value"
+      )
 
     # Plot
     ggplot2::ggplot(data_long, aes(x = age, y = value, color = percentile)) +
       ggplot2::geom_line(linewidth = 0.2) +
       ggplot2::geom_text(
         data = data_long %>% group_by(percentile) %>% slice_max(age, n = 1),
-        aes(label = label_map[percentile]),  # use mapped labels
+        aes(label = label_map[percentile]), # use mapped labels
         size = 6,
         hjust = -0.2
       ) +
       ggplot2::scale_color_manual(
-        values = rep("blue", 7)  # all lines same color
+        values = rep("blue", 7) # all lines same color
       ) +
-      ggplot2::scale_x_continuous(breaks = seq(min(data_long$age), max(data_long$age), by = (max(data_long$age) - min(data_long$age))/4),
-                         limits = c(min(data_long$age), max(data_long$age) + (max(data_long$age) - min(data_long$age))/8)) +
-      ggplot2::abs(
-      title = title,
-      x = "Age (years)",
-      y = value
-    ) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      panel.grid.minor = element_blank(),
-      text = element_text(size = 24),
-      plot.title = element_text(size = 24),
-      panel.border = element_rect(fill = NA, colour = "black"),
-      legend.position = "none",
-      axis.title = element_text(size = 24),
-      axis.text = element_text(size = 24),
-      axis.title.y = element_blank(),
-      aspect.ratio = 1/2
-    )
+      ggplot2::scale_x_continuous(
+        breaks = seq(min(data_long$age), max(data_long$age), by = (max(data_long$age) - min(data_long$age)) / 4),
+        limits = c(min(data_long$age), max(data_long$age) + (max(data_long$age) - min(data_long$age)) / 8)
+      ) +
+      ggplot2::labs(
+        title = title,
+        x = "Age (years)",
+        y = value
+      ) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        panel.grid.minor = element_blank(),
+        text = element_text(size = 24),
+        plot.title = element_text(size = 24),
+        panel.border = element_rect(fill = NA, colour = "black"),
+        legend.position = "none",
+        axis.title = element_text(size = 24),
+        axis.text = element_text(size = 24),
+        axis.title.y = element_blank(),
+        aspect.ratio = 1 / 2
+      )
   } else {
-  # Display empty plot
-  ggplot2::ggplot() +
-    ggplot2::labs(
-      title = "No data available"
-    ) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      text = element_text(size = 24),
-      plot.title = element_text(size = 24, hjust = 0.5),
-      aspect.ratio = 1/2
-    )
+    # Display empty plot
+    ggplot2::ggplot() +
+      ggplot2::labs(
+        title = "No data available"
+      ) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        text = element_text(size = 24),
+        plot.title = element_text(size = 24, hjust = 0.5),
+        aspect.ratio = 1 / 2
+      )
   }
 }
--- script.R
+++
@@ -1,4 +0,0 @@
-# your_filename = "example_file.xlsx"
-# compute_zscores_file(filename = your_filename)
-
-shiny::runApp("app")
