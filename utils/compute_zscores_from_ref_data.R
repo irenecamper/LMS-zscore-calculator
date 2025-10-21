27ceae71d4e62fd1cf01824d3389e16490d2f531
irenita <irenerentzat@email.com>
2025-10-21 16:17:16 +0200

    feat: Suppress messages

--- utils/compute_zscores_from_ref_data.R
+++ utils/compute_zscores_from_ref_data.R
@@ -12,43 +12,46 @@
 #' @return A data frame with additional `zscore_<variable>` columns.
 #'
 compute_zscores_from_ref_data <- function(data, ref_data, values = c("waist", "hip", "WHR")) {
-
-
-  if (!requireNamespace("gamlss", quietly = TRUE))
+  if (!requireNamespace("gamlss", quietly = TRUE)) {
     stop("Package 'gamlss' must be installed.")
+  }
 
   for (value in values) {
     zscores <- numeric(nrow(data))
     new_col <- paste0("zscore_", value)
-    
-    for (gender_value in c(0, 1)) {
 
+    for (gender_value in c(0, 1)) {
       ref_data_curr <- ref_data |>
         filter(gender == gender_value) |>
         select(PATID, age, !!sym(value), gender) |>
         na.omit()
 
-      gamlss_model <- gamlss::gamlss(
-        as.formula(paste(value, "~ pb(age)")),
-        data = ref_data_curr,
-        family = BCPE)
-      
+      gamlss_model <- suppressWarnings(
+        suppressMessages(gamlss::gamlss(
+          as.formula(paste(value, "~ pb(age)")),
+          data = ref_data_curr,
+          family = gamlss::BCPE
+        ))
+      )
+
       idx <- which(data$gender == gender_value)
-      
-      pred <- gamlss::centiles.pred(
-        gamlss_model,
-        xname = "age",
-        xvalues = data$age[idx],
-        yval = data[[value]][idx],
-        type = "z-scores",
-        data = ref_data_curr
+
+      pred <- suppressWarnings(
+        suppressMessages(gamlss::centiles.pred(
+          gamlss_model,
+          xname = "age",
+          xvalues = data$age[idx],
+          yval = data[[value]][idx],
+          type = "z-scores",
+          data = ref_data_curr
+        ))
       )
 
       zscores[idx] <- pred
     }
-    
+
     data[[new_col]] <- zscores
   }
-  
+
   return(data)
 }
